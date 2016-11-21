2056458ce769f1f3ad511d5e13f731bb47eda8be
davetrainer <trainer.david@gmail.com>
2016-11-21 22:14:51 +0000

    Table updates for cheese and country.

--- WineAndCheese/WineAndCheese.Database/dbo/Tables/Cheese.sql
+++ WineAndCheese/WineAndCheese.Database/dbo/Tables/Cheese.sql
@@ -1,6 +1,11 @@
 ﻿CREATE TABLE [dbo].[Cheese]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [Name] NVARCHAR(100) NOT NULL, 
-    [CountryId] INT NOT NULL
+    [CountryId] INT NOT NULL, 
+    CONSTRAINT [FK_Cheese_Country] FOREIGN KEY ([CountryId]) REFERENCES [Country]([Id])
 )
+
+GO
+
+CREATE INDEX [IX_Cheese_Country] ON [dbo].[Cheese] ([CountryId])
