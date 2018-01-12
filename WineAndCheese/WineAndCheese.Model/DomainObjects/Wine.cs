c52acd84e3d929617a0391469f7f43e1bd2284d2
davetrainer <david.trainer@briggsequipment.co.uk>
2018-01-12 22:02:36 +0000

    WineService added to business logic.

---
+++ WineAndCheese/WineAndCheese.Model/DomainObjects/Wine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineAndCheese.Model.DomainObjects
+{
+    public class Wine
+    {
+        #region Constants
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Events & Delegates
+
+        #endregion
+
+        #region Properties
+
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public int GraoeId { get; set; }
+        public int CountryId { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ WineAndCheese/WineAndCheese.Model/Services/WineService.cs
@@ -0,0 +1,51 @@
+﻿using WineAndCheese.Interfaces;
+using WineAndCheese.Repositories;
+
+namespace WineAndCheese.Model.Services
+{
+    public class WineService
+    {
+        #region Constants
+
+        #endregion
+
+        #region Constructors
+
+        public WineService(IWineRepository wineRepository)
+        {
+            _wineRepository = wineRepository;
+        }
+
+        public WineService() : this(new WineRepository())
+        {
+        }
+
+        #endregion
+
+        #region Events & Delegates
+
+        #endregion
+
+        #region Properties
+
+        private IWineRepository _wineRepository { get; set; }
+        #endregion
+
+        #region Methods
+
+        public DomainObjects.Wine GetById(int id)
+        {
+            var entity = _wineRepository.GetById(id);
+
+            return new DomainObjects.Wine()
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                GraoeId = entity.GrapeId,
+                CountryId = entity.CountryId
+            };
+        }
+
+        #endregion
+    }
+}
